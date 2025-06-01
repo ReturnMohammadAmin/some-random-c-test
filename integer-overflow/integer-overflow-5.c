46f32686549f21ac2354759d2da6cfa8b44a71be
ReturnMohammadAmin <ReturnMohammadAmin@github.com>
2025-06-01 10:02:21 +0330

    fix.

--- integer-overflow/integer-overflow-5.c
+++ integer-overflow/integer-overflow-5.c
@@ -1,8 +1,12 @@
+#include <assert.h>
+#include <stdio.h>
+
 int main() {
-    int a = __VERIFIER_nondet_int(), b = __VERIFIER_nondet_int(), sum = 0;
+    int a, b, sum = 0;
+    scanf("%d%d", &a, &b);
     for (int i = 0; i < a; i++)
         for (int j = 0; j < b ; j++)
             sum += (i + j + 1) % 2;
     if (sum == 0xffffffff)
-        __VERIFIER_error();
+        assert(0);
 }
