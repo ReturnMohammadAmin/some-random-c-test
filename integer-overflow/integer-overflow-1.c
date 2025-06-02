80c0695f201dce19e7634314cf9495d8c96ec3bb
ReturnMohammadAmin <ReturnMohammadAmin@github.com>
2025-06-02 13:40:20 +0330

    fix uninitialized.

--- integer-overflow/integer-overflow-1.c
+++ integer-overflow/integer-overflow-1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int add(int x, int y) {
     return x + y;
@@ -6,6 +7,8 @@ int add(int x, int y) {
 
 int main() {
     int a, b;
+    a = rand();
+    b = rand();
     scanf("%d %d", &a, &b);
     int result = add(a, b);
     printf("%d\n", result);
